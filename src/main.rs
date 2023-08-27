868a4f139e253a03492600b2ae0a020a432a333e
Ramon Buckland <ramon@thebuckland.com>
2023-08-27 23:45:33 +1000

    list and add tasks

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use reqwest;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
 use tokio;
 use serde_yaml;
+use serde_json::json;
 use expanduser::expanduser;
-
-use clap::Parser;
+use structopt::StructOpt;
 
 use reqwest::{
     header::{HeaderMap, ACCEPT, USER_AGENT, AUTHORIZATION},
-    Url, Client
+     Client
 };
 
 #[derive(Debug, Deserialize)]
@@ -36,13 +36,29 @@ struct Issue {
     html_url: String,
 }
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[derive(StructOpt, Debug)]
 struct Args {
-    #[arg(short, long, default_value = "~/.config/tskmstr/tskmstr.config.yml")]    
+    #[structopt(short, long)]
+    debug: bool,
+
+    #[structopt(short, long, default_value = "~/.config/tskmstr/tskmstr.config.yml")]    
     config: String,
+
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
 }
 
+#[derive(StructOpt, Debug)]
+enum Command {
+    #[structopt(about = "Add a new issue to the default repository")]
+    Add {
+        #[structopt()]
+        title: String,
+        
+        #[structopt()]
+        details: String,
+    }
+}
 
 pub fn construct_header(token: &str) -> HeaderMap {
     let mut headers = HeaderMap::new();
@@ -55,17 +71,8 @@ pub fn construct_header(token: &str) -> HeaderMap {
     return headers;
 }
 
+async fn list_tasks(github_config: &GitHubConfig) -> Result<(), Box<dyn std::error::Error>> {
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    let args = Args::parse();
-
-    // Read the repository configuration from YAML
-    let config_file = std::fs::read_to_string(expanduser(args.config)?)?;
-    let config: RepositoryConfig = serde_yaml::from_str(&config_file)?;
-
-    let github_config = &config.github_com;
     let client = Client::new();
 
     for repo in &github_config.repositories {
@@ -91,6 +98,67 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             );
         }
     }
+    Ok(())
+}
+
+async fn add_new_task(github_config: &GitHubConfig, title: &str, details: &str) -> Result<(), Box<dyn std::error::Error>> {
+
+    let client = Client::new();
+
+    let default_repo = github_config
+        .repositories
+        .iter()
+        .find(|repo| repo.default.unwrap_or(false))
+        .expect("No default repository found");
+
+    let add_url = format!(
+        "https://api.github.com/repos/{}/{}/issues",
+        default_repo.owner, default_repo.repo
+    );
+
+    let issue_details = json!({
+        "title": title,
+        "body": details,
+    });
+
+    let response = client
+        .post(&add_url)
+        .headers(construct_header(&github_config.token))
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
+        .body(issue_details.to_string())
+        .send().await?;
+
+    if response.status().is_success() {
+        let issue: Issue = response.json::<Issue>().await?;
+        println!("New issue created:");
+        println!("Title: {}", issue.title);
+        println!("URL: {}", issue.html_url);
+    } else {
+        println!(
+            "Error: Unable to create issue. Status: {:?}",
+            response.status()
+        );
+    }
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let args = Args::from_args();
+
+    // Read the repository configuration from YAML
+    let config_file = std::fs::read_to_string(expanduser(&args.config)?)?;
+    let config: RepositoryConfig = serde_yaml::from_str(&config_file)?;
+
+    let github_config = &config.github_com;
+    match args.cmd {
+
+        Some(Command::Add { title, details }) => add_new_task(&github_config, &title, &details).await?,
+        None => list_tasks(&github_config).await?,
+
+    };
+
 
     Ok(())
 }
