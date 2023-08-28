154f90f8769223d4fcbe153b5d0590aa2020f70e
Ramon Buckland <ramon@thebuckland.com>
2023-08-28 23:09:37 +1000

    added gitlab issue pulling

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use serde_yaml;
 use serde_json::json;
 use expanduser::expanduser;
 use structopt::StructOpt;
+use log::{debug, info, warn, error};
+
+use anyhow::Result;
 
 use std::collections::HashMap;
 
@@ -14,21 +17,49 @@ use reqwest::{
 };
 
 #[derive(Debug, Deserialize)]
-struct RepositoryConfig {
+struct AppConfig {
+
+    debug: Option<bool>,
+
     #[serde(rename = "github.com")]
-    github_com: GitHubConfig,
+    github_com: Option<GitHubConfig>,
+
+    #[serde(rename = "gitlab.com")]
+    gitlab_com: Option<GitLabConfig>,
+}
+
+impl Default for AppConfig {
+    fn default() -> AppConfig {
+        AppConfig {
+            debug: None,
+            github_com: None,
+            gitlab_com: None,
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
 struct GitHubConfig {
     token: String,
-    repositories: Vec<Repository>,
+    repositories: Vec<GitHubRepository>,
 }
 
 #[derive(Debug, Deserialize)]
-struct Repository {
+struct GitLabConfig {
+    token: String,
+    repositories: Vec<GitLabRepository>,
+}
+
+#[derive(Debug, Deserialize)]
+struct GitHubRepository {
     owner: String,
     repo: String,
+    default: Option<bool>,
+}
+
+#[derive(Debug, Deserialize)]
+struct GitLabRepository {
+    project_id: String,
     default: Option<bool>,
 }
 
@@ -45,6 +76,20 @@ struct Issue {
 #[derive(Debug, Deserialize, Clone)]
 struct Label {
     name: String,
+}
+
+
+// New type for GitLab labels
+#[derive(Debug, Deserialize, Clone)]
+struct GitLabLabel(String);
+
+#[derive(Debug, Deserialize, Clone)]
+struct GitLabIssue {
+    title: String,
+    web_url: String,
+
+    // Use the new GitLabLabel type for tags
+    labels: Vec<GitLabLabel>,
 }
 
 #[derive(StructOpt, Debug)]
@@ -89,8 +134,9 @@ pub fn construct_header(token: &str) -> HeaderMap {
     return headers;
 }
 
-async fn list_tasks(github_config: &GitHubConfig) -> Result<(), Box<dyn std::error::Error>> {
+async fn collect_tasks_from_github(github_config: &GitHubConfig) -> Result<Vec<Issue>, anyhow::Error> {
     let client = Client::new();
+    let mut all_issues = Vec::new();  // Create a vector to collect all issues
 
     for repo in &github_config.repositories {
         let url = format!("https://api.github.com/repos/{}/{}/issues", repo.owner, repo.repo);
@@ -105,31 +151,7 @@ async fn list_tasks(github_config: &GitHubConfig) -> Result<(), Box<dyn std::err
             let body = response.text().await?;
 
             let issues: Vec<Issue> = serde_json::from_str(&body)?;
-
-            // Group issues by tags
-            let mut issues_by_tags: HashMap<String, Vec<Issue>> = HashMap::new();
-            for issue in &issues {
-                for tag in &issue.tags {
-                    let tag_name = &tag.name;
-                    issues_by_tags
-                        .entry(tag_name.to_string())
-                        .or_insert_with(Vec::new)
-                        .push(issue.clone());
-                }
-            }
-
-
-            for (tag, tag_issues) in &issues_by_tags {
-                println!("Tag: {}", tag);
-                println!("{:-<40}", "-"); // Divider line
-        
-                for issue in tag_issues {
-                    println!(" - {}", issue.title);
-                }
-        
-                println!(); // Empty line between tags
-            }
-
+            all_issues.extend(issues);  // Add the collected issues to the vector
 
         } else {
             println!(
@@ -140,9 +162,103 @@ async fn list_tasks(github_config: &GitHubConfig) -> Result<(), Box<dyn std::err
             );
         }
     }
-    Ok(())
+    Ok(all_issues)  // Return the vector of collected issues
+
 }
 
+
+async fn collect_tasks_from_gitlab(gitlab_config: &GitLabConfig) -> Result<Vec<Issue>, anyhow::Error> {
+    let client = Client::new();
+    let mut all_issues = Vec::new();
+
+    for repo in &gitlab_config.repositories {
+        let url = format!("https://gitlab.com/api/v4/projects/{}/issues", repo.project_id);
+
+        let response = client
+            .get(&url)
+            .header("PRIVATE-TOKEN", gitlab_config.token.as_str())
+            .send()
+            .await?;
+
+        if response.status().is_success() {
+            let body = response.text().await?;
+            debug!("{}", body);
+
+            let gitlab_issues: Vec<GitLabIssue> = serde_json::from_str(&body)?;
+            // Convert GitLab issues to the internal Issue representation
+            let issues = gitlab_issues.into_iter().map(|gitlab_issue| {
+                Issue {
+                    title: gitlab_issue.title,
+                    html_url: gitlab_issue.web_url,
+                    tags: gitlab_issue.labels.into_iter().map(|label| Label { name: label.0 }).collect(),
+                }
+            });
+
+            all_issues.extend(issues);
+
+        } else {
+            println!(
+                "Error: Unable to fetch issues for project_id {}. Status: {:?}",
+                repo.project_id,
+                response.status()
+            );
+        }
+    }
+
+    Ok(all_issues)
+}
+
+fn display_tasks_in_table(issues: &Vec<Issue>) -> Result<(), anyhow::Error> {
+
+                // Group issues by tags
+                let mut issues_by_tags: HashMap<String, Vec<Issue>> = HashMap::new();
+                for issue in issues {
+                    for tag in &issue.tags {
+                        let tag_name = &tag.name;
+                        issues_by_tags
+                            .entry(tag_name.to_string())
+                            .or_insert_with(Vec::new)
+                            .push(issue.clone());
+                    }
+                }
+    
+    
+                for (tag, tag_issues) in &issues_by_tags {
+                    println!("Tag: {}", tag);
+                    println!("{:-<40}", "-"); // Divider line
+            
+                    for issue in tag_issues {
+                        println!(" - {}", issue.title);
+                    }
+            
+                    println!(); // Empty line between tags
+                }
+
+                Ok(())
+}
+
+async fn aggregate_and_display_all_tasks(
+    github_config: &Option<GitHubConfig>,
+    gitlab_config: &Option<GitLabConfig>,
+) -> Result<(), Box<dyn std::error::Error>> {
+
+
+    let mut all_issues = Vec::new();
+
+    if let Some(github_config) = github_config {
+        let github_tasks = collect_tasks_from_github(github_config).await?;
+        all_issues.extend(github_tasks);
+    }
+
+    if let Some(gitlab_config) = gitlab_config {
+        let gitlab_tasks = collect_tasks_from_gitlab(gitlab_config).await?;
+        all_issues.extend(gitlab_tasks);
+    }
+
+    let _ = display_tasks_in_table(&all_issues);
+
+    Ok(())
+}
 
 
 async fn add_new_task(github_config: &GitHubConfig, title: &str, details: &str, tags: &Option<Vec<String>>) -> Result<(), Box<dyn std::error::Error>> {
@@ -193,16 +309,27 @@ async fn add_new_task(github_config: &GitHubConfig, title: &str, details: &str, 
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args = Args::from_args();
-
     // Read the repository configuration from YAML
     let config_file = std::fs::read_to_string(expanduser(&args.config)?)?;
-    let config: RepositoryConfig = serde_yaml::from_str(&config_file)?;
+    let config: AppConfig = serde_yaml::from_str(&config_file)?;
+
+
+    // Initialize your logger
+    if args.debug || config.debug.is_some() {
+        // Set up the logger with the desired log level
+        simple_logger::init_with_level(log::Level::Debug).expect("Failed to initialize logger");
+    } else {
+        // Initialize the logger with a default log level
+        simple_logger::init_with_level(log::Level::Info).expect("Failed to initialize logger");
+    }    
+
 
     let github_config = &config.github_com;
+    let gitlab_config = &config.gitlab_com;
     match args.cmd {
 
-        Some(Command::Add { title, details, tags }) => add_new_task(&github_config, &title, &details, &tags).await?,
-        None => list_tasks(&github_config).await?,
+        Some(Command::Add { title, details, tags }) => add_new_task(&github_config.as_ref().unwrap(), &title, &details, &tags).await?,
+        None => aggregate_and_display_all_tasks(&github_config, &gitlab_config).await?,
 
     };
 
