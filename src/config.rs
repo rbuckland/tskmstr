7048598c260c5b0cddebd0f52c9409140673be11
Ramon Buckland <ramon@thebuckland.com>
2023-09-26 15:58:00 +1000

    feat: add priority grouping

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,6 @@
+use std::collections::HashSet;
+use std::hash::Hash;
+
 use serde::Deserialize;
 
 use crate::providers::github::model::{GitHubConfig, GitHubRepository};
@@ -12,6 +15,8 @@ pub struct AppConfig {
 
     pub colors: Colors,
 
+    pub labels: LabelConfig,
+
     #[serde(rename = "github.com")]
     pub github_com: Option<GitHubConfig>,
 
@@ -19,6 +24,12 @@ pub struct AppConfig {
     pub gitlab_com: Option<GitLabConfig>,
 
     pub o365: Option<O365Config>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct LabelConfig {
+    pub priority_labels: HashSet<String>,
+    pub priority_timeframe: Option<String>
 }
 
 /// there is only one default "place" we will create tasks into
@@ -79,6 +90,7 @@ impl Default for AppConfig {
             debug: None,
             github_com: None,
             gitlab_com: None,
+            labels: LabelConfig { priority_labels: HashSet::new(), priority_timeframe: None },
             o365: None,
             colors: Colors {
                 issue_id: "magenta".to_string(),
--- src/output.rs
+++ src/output.rs
@@ -1,49 +1,170 @@
-use std::collections::HashMap;
-use crate::{providers::common::model::Issue, config::AppConfig};
-use crate::providers::gitlab::model::GitLabConfig;
-use crate::providers::github::model::GitHubConfig;
-use crate::providers::github::methods::collect_tasks_from_github;
-use crate::providers::gitlab::methods::collect_tasks_from_gitlab;
 use crate::config::Colors;
-use colored::{Colorize, Color};
+use crate::providers::github::methods::collect_tasks_from_github;
+use crate::providers::github::model::GitHubConfig;
+use crate::providers::gitlab::methods::collect_tasks_from_gitlab;
+use crate::providers::gitlab::model::GitLabConfig;
+use crate::{config::AppConfig, providers::common::model::Issue};
+use colored::{Color, Colorize};
 use std::str::FromStr;
 
-pub fn display_tasks_in_table(issues: &Vec<Issue>, colors: &Colors) -> Result<(), anyhow::Error> {
-    // Group issues by tags
-    let mut issues_by_tags: HashMap<String, Vec<Issue>> = HashMap::new();
-    for issue in issues {
-        for tag in &issue.tags {
-            let tag_name = &tag.name;
-            issues_by_tags
-                .entry(tag_name.to_string())
+use std::collections::{HashMap, HashSet};
+
+// pub fn display_tasks_in_table(issues: &Vec<Issue>, colors: &Colors) -> Result<(), anyhow::Error> {
+//     // Group issues by tags
+//     let mut issues_by_tags: HashMap<String, Vec<Issue>> = HashMap::new();
+//     for issue in issues {
+//         for tag in &issue.tags {
+//             let tag_name = &tag.name;
+//             issues_by_tags
+//                 .entry(tag_name.to_string())
+//                 .or_insert_with(Vec::new)
+//                 .push(issue.clone());
+//         }
+//     }
+
+//     for (tag, tag_issues) in &issues_by_tags {
+//         println!("Tag: {}", tag.color(Color::from_str(&colors.tags).unwrap()));
+//         println!("{:-<40}", "-"); // Divider line
+
+//         for issue in tag_issues {
+//             let tags = format!(
+//                 "({})",
+//                 issue
+//                     .tags
+//                     .iter()
+//                     .map(|t| t.name.clone())
+//                     .collect::<Vec<String>>()
+//                     .join(", ")
+//             );
+//             println!(
+//                 " - {} {} {}",
+//                 issue.id.color(Color::from_str(&colors.issue_id).unwrap()),
+//                 issue.title.color(Color::from_str(&colors.title).unwrap()),
+//                 tags.color(Color::from_str(&colors.tags).unwrap())
+//             );
+//         }
+
+//         println!(); // Empty line between tags
+//     }
+
+//     Ok(())
+// }
+
+// Function to group tasks by labels, excluding priority labels
+fn group_tasks_by_labels(
+    issues: &Vec<Issue>,
+    priority_labels: &HashSet<String>,
+) -> HashMap<String, Vec<Issue>> {
+    issues
+        .iter()
+        .filter(|issue| {
+            issue
+                .tags
+                .iter()
+                .all(|tag| !priority_labels.contains(&tag.name))
+        })
+        .fold(HashMap::new(), |mut acc, issue| {
+            let labels_except_priority: Vec<String> = issue
+                .tags
+                .iter()
+                .filter(|tag| !priority_labels.contains(&tag.name))
+                .map(|tag| tag.name.clone())
+                .collect();
+
+            let group_key = labels_except_priority.join(", ");
+
+            acc.entry(group_key)
                 .or_insert_with(Vec::new)
                 .push(issue.clone());
+            acc
+        })
+}
+
+fn group_tasks_by_priority_labels(
+    issues: &Vec<Issue>,
+    priority_labels: &HashSet<String>,
+) -> Vec<Issue> {
+    issues
+        .iter()
+        .filter(|issue| {
+            issue
+                .tags
+                .iter()
+                .any(|tag| priority_labels.contains(&tag.name))
+        })
+        .cloned()
+        .collect()
+}
+
+pub fn display_tasks_in_table(
+    issues: &Vec<Issue>,
+    colors: &Colors,
+    priority_labels: &HashSet<String>,
+) -> Result<(), anyhow::Error> {
+    let mut grouped_tasks = group_tasks_by_labels(issues, priority_labels);
+    let priority_tasks = group_tasks_by_priority_labels(issues, priority_labels);
+
+    // Sort the groups by priority, moving the priority group to the front
+    grouped_tasks.remove(""); // Remove the empty key
+    let mut groups: Vec<_> = grouped_tasks.iter().collect();
+    groups.sort_by(|(a, _), (b, _)| {
+        if a == &"priority" {
+            std::cmp::Ordering::Less
+        } else if b == &"priority" {
+            std::cmp::Ordering::Greater
+        } else {
+            a.cmp(b)
         }
+    });
+
+    // Display priority tasks
+    let priority_labels_str = &priority_labels.iter().cloned().collect::<Vec<String>>().join(", ");
+    println!("Priority: {}", priority_labels_str.color(Color::from_str(&colors.tags).unwrap()));            
+    println!("{:-<40}", "-"); // Divider line
+    for issue in &priority_tasks {
+        let tags = format!(
+            "({})",
+            issue
+                .tags
+                .iter()
+                .map(|t| t.name.clone())
+                .collect::<Vec<String>>()
+                .join(", ")
+        );
+        println!(
+            " - {} {} {}",
+            issue.id.color(Color::from_str(&colors.issue_id).unwrap()),
+            issue.title.color(Color::from_str(&colors.title).unwrap()),
+            tags.color(Color::from_str(&colors.tags).unwrap())
+        );
     }
 
-    for (tag, tag_issues) in &issues_by_tags {
-        println!("Tag: {}", tag.color(Color::from_str(&colors.tags).unwrap()));
-        println!("{:-<40}", "-"); // Divider line
+    println!();
 
-        for issue in tag_issues {
-            let tags = format!(
-                "({})",
-                issue
-                    .tags
-                    .iter()
-                    .map(|t| t.name.clone())
-                    .collect::<Vec<String>>()
-                    .join(", ")
-            );
-            println!(
-                " - {} {} {}",
-                issue.id.color(Color::from_str(&colors.issue_id).unwrap()),
-                issue.title.color(Color::from_str(&colors.title).unwrap()),
-                tags.color(Color::from_str(&colors.tags).unwrap())
-            );
+    // Display grouped tasks
+    for (group, group_issues) in groups {
+        if !group.is_empty() {
+            println!("Tag: {}", group.color(Color::from_str(&colors.tags).unwrap()));            
+            println!("{:-<40}", "-"); // Divider line
+            for issue in group_issues {
+                let tags = format!(
+                    "({})",
+                    issue
+                        .tags
+                        .iter()
+                        .map(|t| t.name.clone())
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                );
+                println!(
+                    " - {} {} {}",
+                    issue.id.color(Color::from_str(&colors.issue_id).unwrap()),
+                    issue.title.color(Color::from_str(&colors.title).unwrap()),
+                    tags.color(Color::from_str(&colors.tags).unwrap())
+                );
+            }
+            println!();
         }
-
-        println!(); // Empty line between tags
     }
 
     Ok(())
@@ -61,13 +182,12 @@ pub async fn aggregate_and_display_all_tasks(
         all_issues.extend(github_tasks);
     }
 
-    if let Some(gitlab_config) = &config.gitlab_com  {
+    if let Some(gitlab_config) = &config.gitlab_com {
         let gitlab_tasks = collect_tasks_from_gitlab(gitlab_config).await?;
         all_issues.extend(gitlab_tasks);
     }
 
-    let _ = display_tasks_in_table(&all_issues, &colors);
+    let _ = display_tasks_in_table(&all_issues, &colors, &config.labels.priority_labels);
 
     Ok(())
 }
-
