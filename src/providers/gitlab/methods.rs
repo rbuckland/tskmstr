9bd1439c18fdbf4e1630a0f11b6f1fa136860d64
Ramon Buckland <ramon@thebuckland.com>
2023-09-30 11:11:56 +1000

    feat: can optionally select  one provider

--- src/providers/gitlab/methods.rs
+++ src/providers/gitlab/methods.rs
@@ -24,11 +24,18 @@ pub fn construct_gitlab_header(token: &str) -> HeaderMap {
 
 pub async fn collect_tasks_from_gitlab(
     gitlab_config: &GitLabConfig,
+    provider_id: &Option<String>,
 ) -> Result<Vec<Issue>, anyhow::Error> {
     let client = Client::new();
     let mut all_issues = Vec::new();
 
-    for (idx, repo) in gitlab_config.repositories.iter().enumerate() {
+    for (idx, repo) in gitlab_config
+    .repositories
+    .iter()
+    .filter(|&r| provider_id.is_none() || provider_id.as_deref().is_some_and(|p| r.id == p))
+    .enumerate()
+{
+
         let url = format!(
             "https://gitlab.com/api/v4/projects/{}/issues?state=opened",
             repo.project_id
@@ -47,7 +54,7 @@ pub async fn collect_tasks_from_gitlab(
             let gitlab_issues: Vec<GitLabIssue> = serde_json::from_str(&body)?;
             // Convert GitLab issues to the internal Issue representation
             let issues = gitlab_issues.into_iter().map(|gitlab_issue| Issue {
-                id: format!("{}{}/{}", SHORT_CODE_GITLAB, idx, gitlab_issue.iid),
+                id: format!("{}/{}", repo.id, gitlab_issue.iid),
                 title: gitlab_issue.title,
                 html_url: gitlab_issue.web_url,
                 tags: gitlab_issue
